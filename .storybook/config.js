0a626b599054bf56998312a355b74a776fe0b7e6
sw-yx <swyx@swyx.io>
2018-07-04 05:22:23 -0400

    done

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,6 +3,7 @@ import { configure } from '@storybook/react';
 const req = require.context('../src', true, /.stories.js$/);
 
 function loadStories() {
+  require('./welcomeStory');
   req.keys().forEach(file => req(file));
 }
 
